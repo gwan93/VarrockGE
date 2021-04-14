5d9c53910b2f04250fcfbbeb63802d24cbc5450f
Ray Yiu <rayyiu66@gmail.com>
2021-04-14 16:22:25 -0700

    fixed collections array and state

--- src/AuthProvider.js
+++ src/AuthProvider.js
@@ -1,7 +1,7 @@
 import { createContext, useState, useEffect } from 'react';
 import uuid from 'react-uuid';
 import axios from 'axios';
-import history  from './History';
+import history from './History';
 import { Cookies } from 'react-cookie';
 
 export default function AuthProvider(props) {
@@ -34,23 +34,23 @@ export default function AuthProvider(props) {
   // Perform login process for the user & save authID, etc
   const loginUser = function (email, password) {
     axios.post('/login', { email, password })
-    .then(response => {
-      setState(prev => ({
-        ...prev,
-        user: {
-          balance: response.data.balance,
-          id: response.data.id,
-          isadmin: response.data.isadmin,
-          email: response.data.email
-        }
-      }))
-      setUser({ email, id, name: "Test User" })
-      setAuth(true)
-      history.push("/widgets");
-    })
-    .catch(err => {
-      console.log('Your credentials were not valid. Please try again', err)
-    })
+      .then(response => {
+        setState(prev => ({
+          ...prev,
+          user: {
+            balance: response.data.balance,
+            id: response.data.id,
+            isadmin: response.data.isadmin,
+            email: response.data.email
+          }
+        }))
+        setUser({ email, id, name: "Test User" })
+        setAuth(true)
+        history.push("/widgets");
+      })
+      .catch(err => {
+        console.log('Your credentials were not valid. Please try again', err)
+      })
   };
 
 
@@ -67,33 +67,61 @@ export default function AuthProvider(props) {
       axios.get('/user/1/collections'),
       axios.get('/widgets'),
     ])
-    .then(all => {
-      // Getting response
-      console.log('AHHHHHHHIMLOGGING', all)
-      const [userData, collectionsData, widgetsData] = all
-      console.log(userData.data);
-      console.log(collectionsData.data);
-      // Set state based on the response
-      setState(prev => ({
-        ...prev,
-        collections: collectionsData.data,
-        widgets: widgetsData.data
+      .then(all => {
+        // Getting response
+        console.log('AHHHHHHHIMLOGGING', all)
+        const [userData, collectionsData, widgetsData] = all
+        console.log(userData.data);
+        console.log(collectionsData.data);
+        console.log("lewidget", widgetsData)
+        // Set state based on the response
+        setState(prev => ({
+          ...prev,
+          collections: collectionsData.data,
+          widgets: widgetsData.data
 
-      }))
-      for (const id in collectionsData.data) {
-        console.log('id is', id)
-        axios.get(`/user/1/collections/${collectionsData.id}`)
-        .then(response => {
-          console.log('idcollectionsthingforgio', response.data)
-          setState(prev => ({
-            ...prev,
-          }))
-        })
-      }
-    })
-    .catch(err => {
-      console.log('@@@@@@@@@@@@@@@@', err)
-    })
+        }))
+        //line below retrieves all collections for a given user. 
+        const collectionPromises = collectionsData.data.map(aCollectionData => axios.get(`/user/1/collections/${aCollectionData.id}`));
+        Promise.all(collectionPromises)
+          .then(collectionPromisesResults => {
+            //collectionPromisesResults contains all collections in an array of axios responses.
+            console.log("collectionPromises", collectionPromisesResults)
+            //build a collection object with the for loop below
+            const stateCollections = []
+            for (const collectionPromiseResult of collectionPromisesResults) {
+              // const collection = collectionPromisesResult.data;
+              //if our collection does not have any widgets, we will need to find a way to define the behaviour because index 0 will not exist.
+              //96 - 100 should actually be returned by the API in this format. We should just have to use line 101. 
+              const collection = {
+                collectionName: collectionPromiseResult.data[0].list_name,
+                collectionDescription: collectionPromiseResult.data[0].list_description,
+                widgets: collectionPromiseResult.data
+              };
+              stateCollections.push(collection);
+            }
+            setState(prev => ({
+              ...prev,
+              collections: stateCollections
+            }));
+            console.log('MYFINALState', state)
+          });
+        // for (const id in collectionsData.data) {
+        //   console.log('id is', id)
+        //   axios.get(`/user/1/collections/${collectionsData.id}`)
+        //     .then(response => {
+        //       console.log('idcollectionsthingforgio', response.data)
+        //       // console.log('id[widget]', id[widget_id])
+        //       setState(prev => ({
+        //         ...prev,
+        //         [id["widgets"]]: response.data
+        //       }))
+        //     })
+        // }
+      })
+      .catch(err => {
+        console.log('@@@@@@@@@@@@@@@@', err)
+      })
   }, []);
 
   // authContext will expose these items
