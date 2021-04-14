8553cb3e8a98f8a204859b88d710bbe7cbe7e127
vagrant <gio_wan@live.ca>
2021-04-14 18:30:09 +0000

    Implement Widget frontend route

--- src/App.js
+++ src/App.js
@@ -1,69 +1,54 @@
 
-import { Router, Route, Switch } from 'react-router-dom';
-import { useEffect } from "react";
+import { Router, Route, Switch, Link } from 'react-router-dom';
+import React, { useEffect } from "react";
 import './App.css';
 import axios from "axios";
 
 import Admin from './screens/admin';
 import Login from './screens/login';
 import Home from './screens/home';
-import Widget from './screens/widget';
-import WidgetID from './screens/widget-id';
+import Widgets from './screens/widgets';
 import User from './screens/user';
-import Collections from './screens/collections';
 import history from './History';
 
+export default function App() {
 
-function App() {
+  useEffect(() => {
 
-  // useEffect(() => {
-
-  //   Promise.all([
-  //     axios.get('/user/:id'),
-  //     axios.get('/user/:id/collections/:id'),
-  //     axios.get('/widget'),
-  //     axios.get('/widget/:id')
-  //   ])
-
-  //   .then(all => {
-  //     console.log('AHHHHHHHIMLOGGING', all)
-  //   })
-  //   .catch(err => {
-  //     console.log('@@@@@@@@@@@@@@@@', err)
-  //   })
-
-
-  // }, []);
-
-
+    Promise.all([
+      axios.get('/user/:id'),
+      axios.get('/user/:id/collections/:id'),
+      axios.get('/widgets'),
+      axios.get('/widgets/:id')
+    ])
+    .then(all => {
+      console.log('AHHHHHHHIMLOGGING', all)
+    })
+    .catch(err => {
+      console.log('@@@@@@@@@@@@@@@@', err)
+    })
+  }, []);
    
   return (
-      <main>
-        <Router history = {history}>
-      <Switch>
-          <Route path="/admin" exact>
-          <Admin
-        
-        /> </Route>
+    <main>
+      <Router history = {history}>
+        <nav>
+          <Link to="/">Home </Link>
+          <Link to="/admin">Admin </Link>
+          <Link to="/login">Login </Link>
+          <Link to="/widgets">Widgets </Link>
+          <Link to="/idk">idk</Link>
+          <Link to="/logout">Logout </Link>
+        </nav>
 
+        <Switch>
+          <Route path="/admin" exact><Admin/></Route>
           <Route path="/login"><Login/> </Route>
-
-          <Route path="/widget" exact><Widget/></Route>
-
-          <Route path="/widget/:id"><WidgetID/></Route>
-
-          <Route path="/user/:id/collections" exact><User/></Route>
-
-          <Route path="/user/:id/collections/:id"><Collections/></Route>
-
+          <Route path="/widgets" component={Widgets}></Route>
+          <Route path="/user/:id/collections" component={User}></Route>
           <Route path="/"><Home/></Route>
-
-           </Switch>
-           </Router>
-        </main>
-        
-    );
-  
+        </Switch>
+      </Router>
+    </main>
+  );
 }
-
-export default App;
--- src/screens/widget-id.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function WidgetID(){
-  
-  return(
-<h3>WidgetID</h3>
-  )
-
-}
