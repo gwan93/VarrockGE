68b2fd76ddb77299dcf998dfb7be42f49df873df
vagrant <gio_wan@live.ca>
2021-04-15 18:04:49 +0000

    Create cart component and route to display all items in cart

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ export default function App() {
       <Navigation />
           <Route path="/admin" component={Admin}></Route>
           <Route path="/widgets" component={Widgets}></Route>
+          <Route path="/cart" component={Cart}></Route>
           <Route path="/user/:id/collections" component={User}></Route>
           <Route path="/cart" component={Cart}></Route>
           </div>
