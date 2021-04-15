e2345684db60051ce696d883bfad479491875ffb
vagrant <gio_wan@live.ca>
2021-04-15 00:14:57 +0000

    Display all widgets when a user clicks on the widget tab

--- src/AuthProvider.js
+++ src/AuthProvider.js
@@ -125,7 +125,7 @@ export default function AuthProvider(props) {
   }, []);
 
   // authContext will expose these items
-  const userData = { auth, user, loginUser, logoutUser };
+  const userData = { state, setState, auth, user, loginUser, logoutUser };  
 
   // We can use this component to wrap any content we want to share this context
   return (
