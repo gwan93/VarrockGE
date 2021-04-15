43211f6733b042a09386321d3246ee62d08281c2
vagrant <gio_wan@live.ca>
2021-04-15 00:14:57 +0000

    Display all widgets when a user clicks on the widget tab

--- src/screens/widgets.jsx
+++ src/screens/widgets.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Link, Route, Switch} from 'react-router-dom';
 import Widget from './widget';
+import { authContext } from '../AuthProvider';
 
 export default function Widgets(){
+  const { state } = useContext(authContext)
+
+  console.log('state.widgets are', state.widgets);
+  console.log(state.widgets[0])
+
+  const widgetList = state.widgets.map((widget) => {
+    console.log(widget['id'])
+    return (
+      <Link to={`/widgets/${widget['id']}`}>Widget {widget['id']} </Link>
+    )
+  })
   return(
     <div>
       <h2>These are all of our Widgets!</h2>
 
       <nav>
-        <Link to="/widgets/1">Product #1</Link>
-        <Link to="/widgets/2">Product #2</Link>
-        <Link to="/widgets/3">Product #3</Link>
-        <Link to="/widgets/4">Product #4</Link>
+        {widgetList}
       </nav>
 
       <Switch>
