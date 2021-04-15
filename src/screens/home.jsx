94f35542f3defb8c06bf976574b7a478dfeb3179
Jesse McDermott <mcdermott.jesse9@gmail.com>
2021-04-15 08:45:28 -0700

    edit home page to fix nav

--- src/screens/home.jsx
+++ src/screens/home.jsx
@@ -1,9 +1,30 @@
 import React, { useEffect }from "react";
+import { useHistory, Link } from "react-router-dom";
 
 export default function Home({setNavbar}) {
+  let history = useHistory();
+  const redirectLogin = () => {
+    history.push('/login')
+  }
+  const redirectWidget = () => {
+    history.push('/widgets')
+  }
   
+  //hides nav bar
   useEffect(() => {
     setNavbar(true);
   });
-  return <h3>Home</h3>;
+  return (
+    <div>
+      <h3>Home Page</h3>
+      <img src="https://i.kym-cdn.com/photos/images/newsfeed/002/054/465/b31.jpg" alt="elbaby"/>
+      <div> 
+      <Link to="/login" className="btn btn-primary">Login</Link>
+      <Link to="/widgets" className="btn btn-primary">Marketplace</Link>
+        <button onClick={redirectLogin}>Login</button>
+        <button onClick={redirectWidget}>Marketplace</button>
+      </div>
+    </div>
+ 
+  );
 }
