c67ea6db99df3b5de6808dcd2601ecd87d7e85df
vagrant <gio_wan@live.ca>
2021-04-16 06:51:53 +0000

    display all the widgets that the selected user currently owns

--- src/screens/user.jsx
+++ src/screens/user.jsx
@@ -1,23 +1,48 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { useParams, Link } from 'react-router-dom';
+import { authContext } from '../AuthProvider';
 
 export default function User(){
 
-  const userID = useParams().id;
+  const userID = Number(useParams().id);
+  const { state } = useContext(authContext);
   const [ userProfile, setUserProfile ] = useState({});
+  const [ widgetOwners, setWidgetOwners ] = useState([]);
   
   // Get request to get all widgets
   useEffect(() => {
     axios.get(`/user/${userID}`)
     .then(response => {
-      console.log(response.data);
       setUserProfile(prev => ({
         ...prev,
         ...response.data
       }));
     })
+
+    axios.get('/widgets/owners')
+    .then(response => {
+      setWidgetOwners(response.data)
+    })
   }, [userID]);
+
+  const usersWidgetsID = [];
+  widgetOwners.filter(widget => {
+    if (widget.user_id === userID) {
+      usersWidgetsID.push(widget.widget_id);
+    }
+  })
+
+  const usersWidgetsDetails = state.widgets.filter(widget => {
+    return usersWidgetsID.includes(widget.id);
+  })
+
+  const displayWidgets = usersWidgetsDetails.map(widget => {
+
+    return(
+      <li><Link to={`/widgets/${widget.id}`}>{widget.name}</Link></li>
+    );
+  })
 
   return(
     <div>
@@ -25,9 +50,10 @@ export default function User(){
       <h2>Wallet Balance: {userProfile.balance}</h2>
       <h2>Admin Status: {String(userProfile.isadmin)}</h2>
       <Link to={`/user/${userID}/collections`}><h2>View {userProfile.email}'s Collections</h2></Link>
-      <h2>These are my widgets</h2>
-      <h3>Link to /widget/1 Widget 1</h3>
-      <h3>Link to /widget/2 Widget 2</h3>
+      <h2>{userProfile.email}'s widgets</h2>
+
+      {displayWidgets}
+
     </div>
   );
 }
