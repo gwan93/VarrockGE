45f98d1081f78a9e1b6f0fe17d5324549f04c665
Jesse McDermott <mcdermott.jesse9@gmail.com>
2021-04-17 16:41:58 -0700

    remove widget once purchased

--- src/screens/widgets.jsx
+++ src/screens/widgets.jsx
@@ -1,36 +1,39 @@
-import React, {useContext} from 'react';
-import {Link, Route, Switch} from 'react-router-dom';
-import Widget from './widget';
-import { authContext } from '../AuthProvider';
+import React, { useContext } from "react";
+import { Link, Route, Switch } from "react-router-dom";
+import Widget from "./widget";
+import { authContext } from "../AuthProvider";
 
-export default function Widgets(){
-  const { state } = useContext(authContext)
+export default function Widgets() {
+  const { state } = useContext(authContext);
 
   const renderWidgetList = () => {
     const widgetList = state.widgets.map((widget) => {
-      return (
-        <Link key={`${widget['id']}`} to={`/widgets/${widget['id']}`}>Widget {widget['id']} </Link>
-      )
-    })
+      if (widget.for_sale_by_owner === false) {
+        return null;
+      } else {
+        return (
+          <Link key={`${widget["id"]}`} to={`/widgets/${widget["id"]}`}>
+            Widget {widget["id"]}{" "}
+          </Link>
+        );
+      }
+    });
 
-  return widgetList
-  }
-  
-  return(
+    return widgetList;
+  };
+
+  return (
     <div>
       <h2>These are all of our Widgets!</h2>
 
-      <nav>
-        {renderWidgetList()}
-      </nav>
+      <nav>{renderWidgetList()}</nav>
 
       <Switch>
-        <Route path="/widgets/:widgetID" component={Widget}/>
+        <Route path="/widgets/:widgetID" component={Widget} />
         <Route path="/widgets">
           <h2>Please select a widget to view</h2>
         </Route>
       </Switch>
     </div>
-  )
-
+  );
 }
