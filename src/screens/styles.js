6e8d2ad2e8b86be2e5b50937548a9c7cf0e571c3
vagrant <gio_wan@live.ca>
2021-04-18 06:11:08 +0000

    Fix img sizing

--- src/screens/styles.js
+++ src/screens/styles.js
@@ -18,7 +18,7 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: 'column'
   },
   cardMedia: {
-    paddingTop: '56.25%' // 16:9
+    // paddingTop: '56.25%' // 16:9
   },
   cardContent: {
     flexGrow: 1
