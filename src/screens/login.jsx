72094d846396c152102f046e4bcdd1f3ea72cee0
Ray Yiu <rayyiu66@gmail.com>
2021-04-19 15:47:36 -0700

    adding css to login

--- src/screens/login.jsx
+++ src/screens/login.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 import { useState, useContext } from 'react';
 import { authContext } from '../AuthProvider';
-import { TextField, Button, makeStyles, Grid } from '@material-ui/core';
+import { TextField, Button, makeStyles, Grid, Typography, Card, CardActions, CardMedia } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   container: {
-   height: "100%"
+   height: "100%",
+   cardGrid: {
+    padding: '20px 0'
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  cardMedia: {
+    // paddingTop: '56.25%' // 16:9
+  },
+  cardContent: {
+    flexGrow: 1
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(3),
+    flexGrow: 10
+  },
+
   },
 }));
 
@@ -35,22 +55,52 @@ export default function Login({setNavbar}) {
   };
 
   return (
-    <div className={classes.container}>
-      <form onSubmit={onSubmit}
-      className={classes.container}
+    <div className={classes.container} maxWidth="xs">
+      <Grid
+      container
+      xs={'2'}
+      direction={'column'}
+      spacing={0, 0, 3, 0}
+      justify={'space-around'}
+      // alignContent={'flex-end'}
+      alignItems={'center'}
+      // spacing={0}
+      // justify={"space-between"}
+      // wrap={'wrap'}
+      // className={classes.container}
+      >    
+       <Grid container
+       spacing={3}
+       justify={'flex-end'}
+       alignItems={'center'}
+              >
+      <Card className={classes.card}>
+          <CardMedia className={classes.cardMedia} title="Image title">
+            <img src={'https://64.media.tumblr.com/64eab0859cf319d597b50a5e15f2b609/6397400c1aa19fe4-b3/s1280x1920/5158b52dcf14390075830667ec15066738ce9277.png'} width="180"/>
+          </CardMedia>
+          </Card>
+              </Grid>
+      <form validate onSubmit={onSubmit}
+      className={classes.form}
       >           
-            <Grid
-            direction={'column'}
-            alignItems={'center'}
-            spacing={2}
-            container 
-            justify={"center"}
-            // className={classes.container}
-            >
-              <h3>Login</h3>
+              <Grid 
+              container
+              direction={'column'}
+              alignContent={'space-between'}  
+              spacing={2}
+              >
+            <Grid item>
+            <Typography
+              variant={'h4'}
+              align={'center'}
+              color={'primary'}
+              >
+                 Login
+              </Typography>
+            </Grid>
             <Grid
             item
-            >
+            >  
             <TextField 
             id="emailInput"
             variant={"filled"}
@@ -62,21 +112,23 @@ export default function Login({setNavbar}) {
             </Grid>
             {/* <label htmlFor="emailInput">Email address</label>
             <input
-              type="email"
-              name="login"
-              className="form-control"
-              id="emailInput"
-              aria-describedby="emailHelp"
-              placeholder="Enter email"
-              value={email}
-              onChange={onEmailChange}
-            ></input> */}
+            type="email"
+            name="login"
+            className="form-control"
+            id="emailInput"
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+            value={email}
+            onChange={onEmailChange}
+          ></input> */}
           <Grid
           item
           >
           <TextField 
             id="passwordInput"
             variant={"filled"}
+            name={"password"}
+            type={"password"}
             label={"Password"}
             value={password}
             onChange={onPasswordChange}
@@ -85,19 +137,21 @@ export default function Login({setNavbar}) {
           </Grid>
             {/* <label htmlFor="passwordInput">Password</label>
             <input
-              type="password"
-              name="password"
-              className="form-control"
-              id="passwordInput"
-              placeholder="Password"
-              value={password}
-              onChange={onPasswordChange}
-            ></input> */}
+            type="password"
+            name="password"
+            className="form-control"
+            id="passwordInput"
+            placeholder="Password"
+            value={password}
+            onChange={onPasswordChange}
+          ></input> */}
         <Grid
         item
         >
           <Button
           variant={"contained"}
+          color={"primary"}
+          fullWidth type="submit"
           > 
         Submit 
           </Button>
@@ -105,8 +159,9 @@ export default function Login({setNavbar}) {
         {/* <button type="submit" className="submit">
           Submit
         </button> */}
-            </Grid>
+      </Grid>
       </form>
+        </Grid>
     </div>
   );
 }
