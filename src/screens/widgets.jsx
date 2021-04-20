63f8cee0107cd21b28d2cb396c66ddf05ba31304
Jesse McDermott <mcdermott.jesse9@gmail.com>
2021-04-19 19:32:42 -0700

    fix filtering

--- src/screens/widgets.jsx
+++ src/screens/widgets.jsx
@@ -2,80 +2,87 @@ import React, { useContext, useEffect, useState } from "react";
 import { Link, Route, Switch } from "react-router-dom";
 import Widget from "./widget";
 import { authContext } from "../AuthProvider";
-import axios from 'axios';
-import { Typography, Card, Button, CardActions, CardContent, CardMedia, CssBaseline, Grid, Container, TextField, Checkbox } from '@material-ui/core';
+import axios from "axios";
+import {
+  Typography,
+  Card,
+  Button,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Container,
+  TextField,
+  Checkbox,
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   container: {
     backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(8, 0, 6)
+    padding: theme.spacing(8, 0, 6),
   },
   button: {
-    marginTop: '40px',
-    marginBottom: '40px'
+    marginTop: "40px",
+    marginBottom: "40px",
   },
   cardGrid: {
-    padding: '20px 0'
+    padding: "20px 0",
   },
   card: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column'
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
   },
   cardMedia: {
     // paddingTop: '56.25%' // 16:9
   },
   cardContent: {
-    flexGrow: 1
+    flexGrow: 1,
   },
   check: {
     flexGrow: 1,
     marginTop: "2em",
-     display: "flex",
-     flexDirection: "row",
-    justifyContent: "space-evenly"
-    
-    
-   },
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+  },
 }));
-
 
 export default function Widgets() {
   const classes = useStyles();
   const { state } = useContext(authContext);
-  const [ filters, setFilters ] = useState({
+  const [filters, setFilters] = useState({
     filterList: [],
-    activeFilters: []
+    activeFilters: [],
   });
-  console.log('state', state.widgets)
+  console.log("state", state.widgets);
   // console.log('filters', filters)
 
   useEffect(() => {
-    Promise.all([
-      axios.get('/rarities'),
-      axios.get('/subcategories')
-    ])
-    .then(all => {
-      const [ rarities, subcategories ] = all;
-      const combinedArray = [...rarities.data, ...subcategories.data];
-      setFilters(prev => ({
-        ...prev,
-        filterList: combinedArray
-      }));
-    });
+    Promise.all([axios.get("/rarities"), axios.get("/subcategories")]).then(
+      (all) => {
+        const [rarities, subcategories] = all;
+        const combinedArray = [...rarities.data, ...subcategories.data];
+        setFilters((prev) => ({
+          ...prev,
+          filterList: combinedArray,
+        }));
+      }
+    );
   }, []);
 
   // For each filter, render a checkbox and the name of the filter
   const renderRarityFilters = () => {
-    const filterList = filters.filterList.map(filter => {
+    const filterList = filters.filterList.map((filter) => {
       return (
         <div key={filter.name}>
-          <input 
+          <input
             id={filter.name}
             type="checkbox"
             checked={filters.activeFilters.includes(filter.name)}
-            onChange={() => onFilterChange(filter.name)}  // eg. filter.name = "Pokemon"
+            onChange={() => onFilterChange(filter.name)} // eg. filter.name = "Pokemon"
           />
           <label htmlFor={filter.name}>{filter.name}</label>
         </div>
@@ -90,16 +97,16 @@ export default function Widgets() {
     // Check all filter checkboxes if at least one filter was not originally checked
     if (filter === "ALL") {
       if (filters.activeFilters.length === filters.filterList.length) {
-        setFilters(prev => ({
+        setFilters((prev) => ({
           ...prev,
-          activeFilters: []
+          activeFilters: [],
         }));
       } else {
-        setFilters(prev => ({
+        setFilters((prev) => ({
           ...prev,
-          activeFilters: filters.filterList.map(filter => filter.name)
+          activeFilters: filters.filterList.map((filter) => filter.name),
         }));
-      };
+      }
     } else {
       // If a filter checkbox is interacted with:
       // Remove it from the filters.activeFilters array if the box is unchecked, OR
@@ -108,17 +115,17 @@ export default function Widgets() {
         const filterIndex = filters.activeFilters.indexOf(filter);
         const newFilter = [...filters.activeFilters];
         newFilter.splice(filterIndex, 1);
-        setFilters(prev => ({
+        setFilters((prev) => ({
           ...prev,
-          activeFilters: newFilter
+          activeFilters: newFilter,
         }));
       } else {
-        setFilters(prev => ({
+        setFilters((prev) => ({
           ...prev,
-          activeFilters: [...filters.activeFilters, filter]
+          activeFilters: [...filters.activeFilters, filter],
         }));
-      };
-    };
+      }
+    }
   };
 
   // Renders all widgets onto the page
@@ -127,12 +134,18 @@ export default function Widgets() {
     // then render every single widget available
     // otherwise, only render the widgets that satisfy the filters in the active filters list
     let filteredList;
-    if (filters.activeFilters.length === 0 || filters.activeFilters.length === filters.filterList.length) {
+    if (
+      filters.activeFilters.length === 0 ||
+      filters.activeFilters.length === filters.filterList.length
+    ) {
       filteredList = state.widgets;
     } else {
-      filteredList = state.widgets.filter(item => {
-        return filters.activeFilters.includes(item.rarity_id) && filters.activeFilters.includes(item.subcategory_id)
-      })
+      filteredList = state.widgets.filter((item) => {
+        return (
+          filters.activeFilters.includes(item.rarity_id) &&
+          filters.activeFilters.includes(item.subcategory_id)
+        );
+      });
     }
 
     // The filteredList may be showing widgets out of order.
@@ -143,117 +156,92 @@ export default function Widgets() {
     // Do a final check to make sure that widget.for_sale_by_owner is true
     // and that the owner does indeed intend to sell that widget
     const widgetList = filteredList
-      .filter(widget => widget.for_sale_by_owner)
-      .map(widget => {
+      .filter((widget) => widget.for_sale_by_owner)
+      .map((widget) => {
         return (
-          <li key={`${widget['id']}`}>
-            <Link to={`/widgets/${widget['id']}`}>
-              {widget.id}. {widget['name']}
-            </Link>
-          </li>
+          <Grid item key={widget.id} xs={12} sm={6} md={4}>
+          <Card className={classes.card}>
+            <CardMedia className={classes.cardMedia} title="Image title">
+              <img src={widget.imgurl} width="298" />
+            </CardMedia>
+            <CardContent>
+              <Typography gutterBottom variant="h5">
+                {widget.name}
+              </Typography>
+              <Typography>
+                WidgetID {widget.id}: {widget.description}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small" color="primary">
+                View
+              </Button>
+              <Button size="small" color="primary">
+                <Link to={`/widgets/${widget.id}`}>Clickme</Link>
+              </Button>
+              <Button size="small" color="primary">
+                Edit
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
         );
       });
-    return widgetList;
+     return widgetList;
   };
 
-  const displayWidgets = state.widgets.map(widget => {
-    console.log(widget.description)
-  return (
-  
-  <Grid item key={widget.id} xs={12} sm={6} md={4}>
-    <Card className={classes.card}>
-      <CardMedia className={classes.cardMedia} title="Image title">
-        <img src={widget.imgurl} width="298"/>
-      </CardMedia>
-      <CardContent>
-        <Typography gutterBottom variant="h5">
-          {widget.name}
-        </Typography>
-        <Typography>
-          WidgetID {widget.id}: {widget.description}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small" color="primary">View</Button>
-        <Button size="small" color="primary"><Link to={`/widgets/${widget.id}`}>Clickme</Link></Button>
-        <Button size="small" color="primary">Edit</Button>
-      </CardActions>
-    </Card>
-  </Grid>
-)
-});
 
-return (
-  <div>
-     <CssBaseline />
+  return (
+    <div>
+      <CssBaseline />
       <main>
         <div className={classes.container}>
           <Container>
-            <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
-            NFT Marketplace
+            <Typography
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              NFT Marketplace
             </Typography>
-        <Typography variant="h5" align="center" color="textPrimary" gutterBottom>
-        Please select a widget to view
+            <Typography
+              variant="h5"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Please select a widget to view
             </Typography>
-            
-            {/* </Grid> */}
-            </Container>
-            </div>
-            <div align="left" direction="row">
-            <Container>
-
-        <Grid className={classes.check}>
-        <input
-               
+          </Container>
+        </div>
+        <div align="left" direction="row">
+          <Container>
+            <Grid className={classes.check}>
+              <input
                 id="myInput"
                 type="checkbox"
                 onChange={() => onFilterChange("ALL")}
-                checked={filters.activeFilters.length === filters.filterList.length}
-                
-        /> 
-        All
-            {renderRarityFilters()}
+                checked={
+                  filters.activeFilters.length === filters.filterList.length
+                }
+              />
+              <label htmlFor="myInput">All</label>
+              {renderRarityFilters()}
             </Grid>
-      <Switch>
-        <Route path="/widgets/:widgetID" component={Widget} />
-        <Route path="/widgets">
-          
-        </Route>
-      </Switch>
-      {/* <nav>{renderWidgetList()}</nav> */}
-        
-        
-            </Container>
-            </div>
-            <Container className={classes.cardGrid} maxWidth="md">
+            <Switch>
+              <Route path="/widgets/:widgetID" component={Widget} />
+              <Route path="/widgets"></Route>
+            </Switch>
+            {/* <nav>{renderWidgetList()}</nav> */}
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
           <Grid container spacing={4}>
-            {displayWidgets}
+            {renderWidgetList()}
           </Grid>
         </Container>
-            </main>
-            </div>
-
-  //      <h2>These are all of our Widgets!</h2>
-
-  // <div>Show all Cards</div>
-  //   <input 
-  //       id="myInput"
-  //       type="checkbox"
-  //       onChange={() => onFilterChange("ALL")}
-  //       checked={filters.activeFilters.length === filters.filterList.length}
-  //     />
-  //     <label htmlFor="myInput">All</label>
-  //     <div>{renderRarityFilters()}</div>
-
-  //     <nav>{renderWidgetList()}</nav>
-
-  //     <Switch>
-  //       <Route path="/widgets/:widgetID" component={Widget} />
-  //       <Route path="/widgets">
-  //         <h2>Please select a widget to view</h2>
-  //       </Route>
-  //     </Switch>
-  //     {displayWidgets}
-  //   </div> 
-  )
-};
+      </main>
+    </div>
+  );
+}
